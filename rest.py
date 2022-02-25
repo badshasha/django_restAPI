eccef9d51217549f9139fb8819eb2c6b54720c1e
shavendra <shavendragoesoft@gmail.com>
2022-02-25 10:40:46 +0530

    add decorator

--- rest.py
+++ rest.py
@@ -2,8 +2,9 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
 from .models import Drink
 from .serializers import DrinkSerializer
+from rest_framework.decorators import api_view
 
-
+@api_view('GET','POST')
 def drinkList(request):
     # get all the drinks
     all_drinks =  Drink.objects.all()
@@ -11,3 +12,5 @@ def drinkList(request):
     # send to client
     serializer = DrinkSerializer(all_drinks, many=True)
     return JsonResponse({'data':serializer.data}, safe=False)
+
+    # add decorator 
