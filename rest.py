603b841f13bf25e38a5ee38deafbd6f12cbc4744
shavendra <shavendragoesoft@gmail.com>
2022-02-25 11:49:18 +0530

    url pattern converting to json

--- rest.py
+++ rest.py
@@ -1,31 +1,11 @@
-@api_view(['GET','PUT','DELETE'])
-def drinkinfo(request,id):
+from django.contrib import admin
+from django.urls import path
+from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
-    # validate information and find the object
-    # try:
-    #     drink = Drink.objects.filter(pk=id)
-    # except Drink.DoesNotExists:
-    #     return Response(status=status.HTTP_404_NOT_FOUND)
-
-    drink = get_object_or_404(Drink,pk=id)
-
-    if request.method == 'GET':
-        serialize = DrinkSerializer(drink)
-        return Response(serialize.data)
-
-
-    elif request.method == 'PUT':
-
-        serializer = DrinkSerializer(drink , data=request.data)
-        
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-
-        else:
-            return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
-
-
-    elif request.method == 'DELETE':        
-        drink.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.drinkList , name='index'),
+    path('drink/<int:id>/', views.drinkinfo , name = "info"),
+]
+urlpatterns = format_suffix_patterns(urlpatterns)
