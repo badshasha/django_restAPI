0006104ae07c1b9c70138a7d9d021ea78ead325d
shavendra <shavendragoesoft@gmail.com>
2022-02-25 10:35:03 +0530

    add views with json

--- rest.py
+++ rest.py
@@ -1,10 +1,13 @@
-INSTALLED_APPS = [
-    'django.contrib.admin',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
-    'django.contrib.staticfiles',
-    'drinks',
-    'rest_framework',
-]
+from django.http import HttpResponse, JsonResponse
+from django.shortcuts import render
+from .models import Drink
+from .serializers import DrinkSerializer
+
+
+def drinkList(request):
+    # get all the drinks
+    all_drinks =  Drink.objects.all()
+
+    # send to client
+    serializer = DrinkSerializer(all_drinks, many=True)
+    return JsonResponse({'data':serializer.data}, safe=False)
